c2891ff063c72f2c43d0658de3c5e86ee9910500
emabishi <frankiemabishi@gmail.com>
2022-10-17 12:32:34 +0300

    Adds new delivery screen

--- App.js
+++ App.js
@@ -9,6 +9,8 @@ import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 import BasketScreen from './screens/BasketScreen';
 import PreparingOrderScreen from './screens/PreparingOrderScreen';
+import DeliveryScreen from './screens/DeliveryScreen';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -25,6 +27,7 @@ export default function App() {
             <Stack.Screen name="Restaurant" component={RestaurantScreen} />
             <Stack.Screen name="Basket" component={BasketScreen} options={{ presentation: 'containedModal', headerShown: false }} />
             <Stack.Screen name="PreparingOrder" component={PreparingOrderScreen} options={{ presentation: 'fullScreenModal', headerShown: false }}/>
+            <Stack.Screen name="Delivery" component={DeliveryScreen} options={{ headerShown: false }}/>
           </Stack.Navigator>
         </TailwindProvider>
     </Provider>
---
+++ screens/DeliveryScreen.js
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class DeliveryScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>DeliveryScreen</Text>
+      </View>
+    )
+  }
+}
+
+export default DeliveryScreen
